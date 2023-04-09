c1892e3db59db7a14ede9023ddcb7e3bf6e9191b
PaulinaOrellana <paplina@gmail.com>
2023-04-09 18:17:04 -0400

    intento de Publicar

--- src/views/Homeprivado.jsx
+++ src/views/Homeprivado.jsx
@@ -1,31 +1,56 @@
 import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext , useEffect} from 'react';
 import Contexto from '../context/Contexto';
 
 
 const Homeprivado = () => {
 
-    const { usuario, setUsuario } = useContext(Contexto);
+    const { usuario, setUsuario, setLstProductos, lstProductos} = useContext(Contexto);
     const navigate =  useNavigate();
 
     const cerrarApp = () =>{
         setUsuario({conectado: false, nombre: ''});
         navigate('/');
-    }
+    } 
+     const cargarDatos = async () => {
+        const res = await fetch ('http://localhost:3000/juegos.json');
+        const data = await res.json();
+
+        setLstProductos([...data]);
+         } 
+
+      useEffect(() => { 
+        cargarDatos();
+       }, [])
+      
 
   return (
     <div>
-        <div style={{display: ' flex', justifyContent:'space-between'}}>
-    <NavLink to="/publicar">Publicar</NavLink>
-    <NavLink to="/perfil">Perfil</NavLink>
+        <div style={{display: 'flex', justifyContent:'space-between'}}>
+          <NavLink to="/publicar">Publicar</NavLink>
+          <NavLink to="/perfil">Perfil</NavLink> 
+          <div>
+              <strong>{usuario.email}</strong>
+              <button type="button" onClick={() => cerrarApp()}>Salir</button>
+            </div>
+       </div>
+
+    <div style={{display:'flex'}}> 
+    {
+            lstProductos.map((producto) => {
+              return (
+                <div style={{width:'25%'}}>
+                
+                 {producto.name} 
+                 
+                  </div>
+              );
+            })}
     </div>
-    <strong>{usuario.email}</strong>
-    <button type="button" onClick={() => cerrarApp()}>Salir</button>
-   
     </div>
     
-  )
-}
+  );
+};
 
-export default Homeprivado
+export default Homeprivado;
--- src/views/Publicar.jsx
+++ src/views/Publicar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import Contexto from '../context/Contexto';
+import { useNavigate } from 'react-router-dom';
 
 const Publicar = () => {
-  return (
-    <div>Publicar</div>
+
+  const{ lstProductos, setLstProductos } = useContext(Contexto);
+
+  const [nombre, setNombre] = useState('');
+
+  const navigate = useNavigate();
+
+  const publicarProducto = () => {
+      setLstProductos([...lstProductos,{name:nombre}]);
+      navigate('/homeprivado'); 
+  }
+
+  return (  
+    <form>
+    <input type="text" onChange={(e) => setNombre(e.target.value)} placeholder='Nombre de Juego'/>
+    <hr/>
+    
+    <button type="button" onClick={() => publicarProducto()}>Publicar</button> 
+ </form>
   )
 }
 
